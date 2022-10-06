7faff70068c3d3300a3f72449ae3913248a47d5f
Cayoten <57970060+cayoten@users.noreply.github.com>
2022-10-06 13:23:01 -0500

    List of changes: - Update packages - Re-word some phrasing parrot.js - Slight typo fix on nullify.js - Introduce mimic.js, a say command

---
+++ commands/general/mimic.js
@@ -0,0 +1,27 @@
+const {SlashCommandBuilder, PermissionsBitField} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("mimic")
+        .setDescription("Says what you tell me to say to the server")
+        .addStringOption(option =>
+            option
+                .setName("message")
+                .setDescription("What you'd like me to say to the server")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+
+        if (!interaction.member.permissions.has([PermissionsBitField.Flags.ManageMessages])) {
+            return interaction.reply({
+                content: "You do not have permission to use this command!",
+                ephemeral: true
+            })
+        }
+
+        //Reply with what the user said
+        interaction.reply({
+            content: interaction.options.getString("message")
+        })
+    }
+}
